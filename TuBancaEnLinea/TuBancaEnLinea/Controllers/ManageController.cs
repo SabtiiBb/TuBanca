e480114e3f7d67591d0c0b746189254ed2b2c0b8
rjjaguilar <santii_mejia@live.com>
2020-09-04 14:24:45 -0600

    Generalizando Cambios

---
+++ TuBancaEnLinea/TuBancaEnLinea/Controllers/ManageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TuBancaEnLinea.Controllers
+{
+    public class ManageController : Controller
+    {
+        // GET: Manage
+        public ActionResult General()
+        {
+            return View();
+        }
+
+        public ActionResult Movimientos()
+        {
+            return View();
+        }
+
+    }
+}
