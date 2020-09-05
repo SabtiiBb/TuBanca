4882f8c36797051fb55f16503a73f0f42dcc2ea3
rjjaguilar <santii_mejia@live.com>
2020-09-04 22:20:11 -0600

    cabios generales

--- TuBancaEnLinea/TuBancaEnLinea/Controllers/AccountController.cs
+++ TuBancaEnLinea/TuBancaEnLinea/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using TuBancaEnLinea.Models;
 
 namespace TuBancaEnLinea.Controllers
 {
@@ -11,6 +13,20 @@ namespace TuBancaEnLinea.Controllers
         // GET: Account
         public ActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                if(model.User == "Admin" && model.Password == "password")
+                {
+                    return RedirectToAction("", "");
+                }
+            }
+
             return View();
         }
     }
